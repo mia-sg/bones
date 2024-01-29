6f20e3e4100450d46fae9fabc766247f25c010b5
mia-sg <144038743+starbound245@users.noreply.github.com>
2024-01-29 20:06:54 +0100

    edits

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {start} = require('apico/server');
+const { start } = require('apico/server');
 
-start(3000, function(p_port){
-    console.log(`Server is running on http://localhost:${p_port}`);
-});
+start( 3000, function(){
+    console.log(` The server is running on http://localhost:3000 `);
+})
